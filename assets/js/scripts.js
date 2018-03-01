65334ae553e8699d391be982b02f9b6cfcd3dd58
iukones <iukones1@gmail.com>
2018-02-28 21:57:25 -0600

    maquetado css y descarga de doodle e imagenes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,7 @@
 function buscar() {
-    var str = document.getElementById('googlebox').value;            
-    str = "https://www.google.com/search?hl=en&source=hp&q=" + str + "&btnG=Buscar&meta=";            
-    var replaced = str.replace(" ","+");            
-    window.location.replace(replaced);            
+    var str = document.getElementById('googlebox').value;
+    str = "https://www.google.com/search?q=" + str;
+    var replaced = str.replace(" ","+");
+    window.location.replace(replaced);
 }
 
-// function redireccionar(){
-//     location.href="http://localhost:8887/google-example/";
-// }
-
-// setTimeout ("redireccionar()", 10000);
