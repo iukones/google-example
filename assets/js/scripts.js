d110b35449267e5c156fa655ec12746c93e691aa
Israel Martinez <iukones1@gmail.com>
2018-02-28 18:40:59 -0600

    se genera script de busqueda y se agregan imagenes de google

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,12 @@
-function buscar()
-        {
-            var str=document.getElementById('googlebox').value;
-            str = "http://www.google.com/search?hl=en&source=hp&q=" + str + "&aq=f&oq=&aqi=";
-            var replaced= str.replace(" ","+");
-            window.location.replace(replaced)
-        }
+function buscar() {
+    var str = document.getElementById('googlebox').value;            
+    str = "https://www.google.com/search?hl=en&source=hp&q=" + str + "&btnG=Buscar&meta=";            
+    var replaced = str.replace(" ","+");            
+    window.location.replace(replaced);            
+}
+
+// function redireccionar(){
+//     location.href="http://localhost:8887/google-example/";
+// }
+
+// setTimeout ("redireccionar()", 10000);
